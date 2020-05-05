12999ae03d257ac0be00b89fdbfe7a5a70fe4ac1
Morgan Harold Bauer <bauer.morgan@gmail.com>
2020-05-05 12:36:22 -0700

    set up org-mode agenda

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;;; Time-stamp: <2019-04-27 15:30:25 Pacific Daylight Time (Morgan Bauer)>
+;;;; Time-stamp: <2020-05-05 12:35:52 PDT (mhb)>
 ;;; init.el --- Summary
 ;;; Commentary:
 
@@ -35,6 +35,7 @@
      (QuakeNet nil nil "Q@CServe.quakenet.org" "auth" t nil nil)
      (SlashNET "NickServ!services@services.slashnet.org" "/msg\\s-NickServ\\s-IDENTIFY\\s-password" "NickServ@services.slashnet.org" "IDENTIFY" nil nil nil)
      (bluenet "" "" "NickServ" "IDENTIFY" nil "" ""))))
+ '(org-agenda-files (quote ("~/sync/org-notes/todo.org")))
  '(package-selected-packages
    (quote
     (flycheck-golangci-lint solidity-mode deft go-tag delight use-package toc-org rg go-mode golint gotest eldoc-overlay-mode company-flx flx flx-ido flx-isearch flycheck flycheck-checkbashisms flycheck-scala-sbt flycheck-swift flycheck-swift3 counsel counsel-bbdb counsel-gtags counsel-osx-app counsel-projectile coverage coverlay ivy company swiper markdown-toc memento go-guru flymd gh-md jekyll-modes markdown-preview-mode markdownfmt ox-gfm vmd-mode yaml-mode zenburn-theme wgrep-ag wget w3 vagrant-tramp vagrant uncrustify-mode ttrss slime-company scala-mode2 rustfmt redshank rbenv rainbow-mode rainbow-identifiers rainbow-delimiters rainbow-blocks racer paredit-everywhere pabbrev osx-pseudo-daemon osx-clipboard osx-browse orglue orglink orgit org-wc org-tracktable org-time-budgets org-readme org-random-todo org-projectile org-presie org-pomodoro org-pdfview org-mobile-sync org-magit org-journal org-gcal org-dotemacs org-doing org-dashboard org-ac ob-sml ob-scala ob-prolog ob-lfe ob-kotlin ob-ipython ob-go o-blog nzenburn-theme nyan-prompt nyan-mode mvn magit-topgit magit-svn magit-stgit magit-gh-pulls magit-gerrit magit-find-file magit-filenotify literate-starter-kit journal helm-company go-scratch go-eldoc go-direx go-autocomplete gnorb flymake-rust flycheck-tip flycheck-rust flycheck-package flycheck-color-mode-line flycheck-clojure exec-path-from-shell ess-R-data-view ercn erc-view-log erc-tweet erc-track-score erc-social-graph erc-image erc-hl-nicks erc-colorize elisp-cache elfeed-web elfeed-org elfeed-goodies electric-spacing electric-operator electric-case eldoro company-web company-go company-c-headers company-auctex code-library cargo bog bbdb2erc auto-highlight-symbol auto-dictionary auto-complete-sage auto-complete-nxml auto-complete-exuberant-ctags auto-complete-clang-async atom-one-dark-theme atom-dark-theme arjen-grey-theme aria2 apache-mode ant ansible annotate ag adjust-parens adaptive-wrap ada-ref-man ada-mode)))
