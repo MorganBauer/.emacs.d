77bf442fe495ac395c28339c7486f8c73a67d9a2
Morgan Harold Bauer <bauer.morgan@gmail.com>
2021-10-02 14:22:59 -0700

    move gc and packaging earlier in startup

--- early-init.el
+++ early-init.el
@@ -1,3 +1,10 @@
-;; this must be done here
+;; this must be done here, as package initialization happens before my
+;; personal configuration takes place.
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; set up packaging
 (require 'package)
 (setq package-enable-at-startup nil)
+
+;; make sure to reload and compile .el if newer than .elc
+(setq load-prefer-newer t)
